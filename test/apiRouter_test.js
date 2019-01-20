fca86feeed79c93275aa183839d3955033fec3b4
y-akamatsu <ak1985tk@gmail.com>
2019-01-20 17:39:06 +0900

    modified apiRouter_test

--- test/apiRouter_test.js
+++ test/apiRouter_test.js
@@ -25,27 +25,52 @@ function testDelete(url, statusCode) {
     .expect(statusCode);
 }
 
+//正常系テストの実装
 describe('GET /api', () => {
   it('GETメソッドの確認', () => {
-    return testGet('/todos/api', 200);
+    return testGet('/api/todos', 200);
   });
 });
 
 describe('POST /api', () => {
   it('POSTメソッドの確認', () => {
-    return testPost('/todos/api', 200);
+    return testPost('/api/todos', 200);
   });
 });
 
 describe('PUT /api', () => {
   it('PUTメソッドの確認', () => {
-    return testPut('/todos/api', 200);
+    return testPut('/api/todos/1', 200);
+  });
+});
+
+describe('DELET /api', () => {
+  it('DELETEメソッドの確認', () => {
+    return testPut('/todos/api/1', 200);
+  });
+});
+
+//異常系テストの実装
+describe('GET /api', () => {
+  it('ステータスコード404になる', () => {
+    return testGet('/todos/api', 404);
+  });
+});
+
+describe('POST /api', () => {
+  it('ステータスコード404になる', () => {
+    return testPost('/todos/api', 404);
+  });
+});
+
+describe('PUT /api', () => {
+  it('ステータスコード404になる', () => {
+    return testPut('/todos/api/1', 404);
   });
 });
 
 describe('DELETE /api', () => {
-  it('DELETEメソッドの確認', () => {
-    return testDelete('/todos/api', 200);
+  it('ステータスコード404になる', () => {
+    return testDelete('/todos/api/1', 404);
   });
 });
-;
