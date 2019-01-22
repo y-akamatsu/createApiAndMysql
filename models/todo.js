b51b40cc06c0be3ab1d6df84daeb84cd86acf40d
y-akamatsu <ak1985tk@gmail.com>
2019-01-22 18:54:36 +0900

    create database todoapi_development

--- models/todo.js
+++ models/todo.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     body: DataTypes.TEXT,
     completed: DataTypes.BOOLEAN
   }, {});
-  Todo.associate = function(models) {
+  Todo.associate = function (models) {
     // associations can be defined here
   };
   return Todo;
