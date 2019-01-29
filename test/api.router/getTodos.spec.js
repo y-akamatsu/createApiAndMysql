90b63b79b1a5dbc7a0dcfec365140e44d2cc12e9
y-akamatsu <ak1985tk@gmail.com>
2019-01-29 22:34:20 +0900

    npm i faker

--- test/api.router/getTodos.spec.js
+++ test/api.router/getTodos.spec.js
@@ -1,29 +1,38 @@
-const assert = require('power-assert');
-const requestHelper = require('../requestHelper');
+const assert = require("power-assert");
+const requestHelper = require("../requestHelper");
+const todoFactory = require("../../models/todo");
+const truncate = require("../truncate");
 
-describe('GET /api/todos', () => {
-  it('APIで取得した値の確認', () => {
-    return requestHelper.requestAPI('get', '/api/todos', 200)
-      .set('Accept', 'application/json')
+describe("GET /api/todos", () => {
+  before(async () => {
+    const promises = [];
+    for (let i = 0; i < 5; i++) {
+      promises.push(todoFactory());
+    }
+    await Promise.all(promises);
+  });
+
+  after(async () => {
+    await truncate();
+  });
+
+  it("API経由で取得したデータの確認", () => {
+    return requestHelper
+      .requestAPI("get", "/api/todos", 200)
+      .set("Accept", "application/json")
       .then(response => {
         const todos = response.body;
-        assert.equal(Array.isArray(todos), true, '配列ではありません');
+
+        assert.equal(Array.isArray(todos), true, "配列ではありません。");
         todos.forEach((todo, index) => {
-          assert.equal(todo.id, index + 1, 'idが正しくありません');
-          assert.equal(typeof todo.title, 'string', 'titleの型が間違っています。');
-          assert.equal(typeof todo.body, 'string', 'bodyの型が間違っています。');
-          assert.equal(typeof todo.completed, 'boolean', 'completedの値が間違っています。');
-          assert.equal(todo.completed, false, 'completedの値はtrueではありません。');
-          assert.equal(typeof todo.createdAt, 'string', 'createdAtの型が間違っています。');
-          assert.equal(typeof todo.updatedAt, 'string', 'updatedAtの型が間違っています。');
+          // DBの各カラムの型チェックの確認
+          assert.equal(typeof todo.id, "number", "idは'number'型ではありません。");
+          assert.equal(typeof todo.title, "string", "titleは'string'型ではありません。");
+          assert.equal(typeof todo.body, "string", "bodyは'string'型ではありません。");
+          assert.equal(typeof todo.completed, "boolean", "completedは'boolean'型ではありません。");
+          assert.equal(typeof todo.createdAt, "string", "createdAtは'string'型ではありません。");
+          assert.equal(typeof todo.updatedAt, "string", "updatedAtは'string'型ではありません。");
         });
       });
-  });
-});
-
-//異常系のテスト
-describe('GET /api/aaaa', () => {
-  it('ステータスコード404になる', () => {
-    return requestHelper.requestAPI('get', '/api/aaaa', 404)
   });
 });
---
+++ test/todo.js
@@ -0,0 +1,15 @@
+const faker = require("faker");
+const index = require("../models/index");
+
+const data = (props = {}) => {
+  const defaultProps = {
+    title: faker.name.title(),
+    body: faker.lorem.sentence(),
+    completed: faker.random.boolean()
+  };
+  return Object.assign({}, defaultProps, props);
+};
+
+const createTodo = async (props = {}) => await index.Todo.create(data(props));
+
+module.exports = createTodo;
