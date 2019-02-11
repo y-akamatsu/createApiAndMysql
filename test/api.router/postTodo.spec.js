0d2ecd679a2dcedd186b9aeb9fc580b3bb24f6fa
y-akamatsu <ak1985tk@gmail.com>
2019-02-11 19:02:44 +0900

    modified postTodo.spec.js

--- test/api.router/postTodo.spec.js
+++ test/api.router/postTodo.spec.js
@@ -3,7 +3,6 @@ const requestHelper = require("../requestHelper");
 const truncate = require("../truncate");
 let id;
 
-//正常系のテスト
 describe("POST /api/todos", () => {
   it("テストコードで作成した値の確認", () => {
     return requestHelper
@@ -11,20 +10,12 @@ describe("POST /api/todos", () => {
       .set("Content-type", "application/json")
       .send(JSON.stringify({title: "titleA", body: "bodyA", completed: false }))
       .then(response => {
-        //データの型のテスト
-        //assert.equal(実際値, 期待値, エラー文);
         assert.equal(typeof response.body.id, "number", "idの型が正しくありません。");
-        assert.equal(response.body.title, "titleA", "titleの型が正しくありません。");
-        assert.equal(response.body.body, "bodyA", "bodyの型が正しくありません。");
-        assert.equal(response.body.completed, false, "completedの型が正しくありません。");
-        assert.equal(typeof response.body.createdAt, "string", "createdAtの型が正しくありません。");
-        assert.equal(typeof response.body.updatedAt, "string", "updatedAtの型が正しくありません。");
-        //データの値のテスト
-        id = response.body.id;
-        assert.equal(response.body.id, id, "idの値が正しくありません。");
         assert.equal(response.body.title, "titleA", "titleの値が正しくありません。");
         assert.equal(response.body.body, "bodyA", "bodyの値が正しくありません。");
         assert.equal(response.body.completed, false, "completedの値が正しくありません。");
+        assert.equal(typeof response.body.createdAt, "string", "createdAtの型が正しくありません。");
+        assert.equal(typeof response.body.updatedAt, "string", "updatedAtの型が正しくありません。");
       });
   });
 });
